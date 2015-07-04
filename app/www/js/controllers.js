55bbe9260bb37663aef82f5233955f5d8ad15f9d
Matt Williams <matwilliams@hotmail.com>
2015-07-04 17:18:56 +0100

    Remove chat and account tabs

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,5 +1,7 @@
 angular.module('starter.controllers', [])
 
+.controller('IntroCtrl', function () {})
+
 .controller('DashCtrl', function ($scope, $location, queryData) {
 
 	$scope.noFields = false;
@@ -37,25 +39,6 @@ angular.module('starter.controllers', [])
 
 
 
-})
-
-.controller('ChatsCtrl', function ($scope, Chats) {
-	// With the new view caching in Ionic, Controllers are only called
-	// when they are recreated or on app start, instead of every page change.
-	// To listen for when this page is active (for example, to refresh data),
-	// listen for the $ionicView.enter event:
-	//
-	//$scope.$on('$ionicView.enter', function(e) {
-	//});
-
-	$scope.chats = Chats.all();
-	$scope.remove = function (chat) {
-		Chats.remove(chat);
-	}
-})
-
-.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-	$scope.chat = Chats.get($stateParams.chatId);
 })
 
 .directive('imageSearch', function (imageSeachFactory) {
