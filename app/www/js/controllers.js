e857fdeb9680c835cf2855ec322c30e2f658d42a
jordanpgl <jpgl@mail.com>
2015-07-04 16:17:48 +0100

    Fix for ng model logic on search page

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,118 +1,90 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+    .controller('DashCtrl', function ($scope) {
+        $scope.noFields = false;
+        $scope.place = [];
+        $scope.searchLocation = function () {
+            if ($scope.startLocation === '' || $scope.targetLocation === '') {
+                $scope.noFields = true;
+            } else {
+                $scope.noFields = false;
+            }
+        };
 
-$scope.noFields = false;
+        $scope.getCurrentLocation = function () {
+            console.log('hello');
+            //geolocation.getLocation().then(function(data){
+            //     $scope.startLocation = {lat:data.coords.latitude, long:data.coords.longitude};
+            //   });
+        };
+    })
 
-$scope.startLocation = '';
-$scope.targetLocation = '';
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-$scope.searchLocation = function() {
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        }
+    })
 
-  console.log($scope.startLocation);
-  console.log($scope.targetLocation);
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
 
+    .directive('imageSearch', function (imageSeachFactory) {
+        return {
+            scope: {
+                keyword: '@keyword'
+            },
+            link: function (scope, element) {
+                var keyword = scope.keyword;
+                imageSeachFactory.getImage(keyword)
+                    .success(function (data, status, headers, config) {
+                        element.append('<img src="' + data.items[0].link + '" style="width: 100%; height: auto" />');
+                    })
+            },
+            template: '<div></div>'
+        }
+    })
 
-if($scope.startLocation === undefined || 
-  $scope.startLocation === '' || 
-  $scope.targetLocation === undefined || 
-  $scope.targetLocation === '') {
+    .controller('RouteCtrl', function ($scope, $ionicSlideBoxDelegate, $q, routes, what3words) {
+        routes.getWayPoints('37.7738571,-122.4102823', '37.7891231,-122.4173545').then(function (wayPoints) {
+            var deferreds = [];
+            for (var wayPointIndex = 0; wayPointIndex < wayPoints.length; wayPointIndex++) {
+                var wayPoint = wayPoints[wayPointIndex];
+                deferreds.push(what3words.getWords(WHAT3WORDS_API_KEY, wayPoint.lat, wayPoint.lon));
+            }
 
-    $scope.noFields = true;
+            $q.all(deferreds).then(function (wordLists) {
+                console.log(wayPoints);
+                console.log(wordLists);
+                $scope.data = {};
+                $scope.data.wayPoints = [];
+                for (var wayPointIndex = 0; wayPointIndex < wayPoints.length; wayPointIndex++) {
+                    var wayPoint = wayPoints[wayPointIndex];
+                    var words = wordLists[wayPointIndex];
+                    $scope.data.wayPoints.push({
+                        lat: wayPoint.lat,
+                        lon: wayPoint.lon,
+                        bearing: wayPoint.bearing,
+                        words: words
+                    });
+                }
+                console.log($scope.data.wayPoints);
+                $ionicSlideBoxDelegate.update();
+            });
+        });
+    })
 
-} else {
-
-
-    $scope.noFields = false;
-
-}
-
-
-
-
-};
-
-$scope.getCurrentLocation = function() {
-
-
-console.log('hello');
-
-//geolocation.getLocation().then(function(data){
- //     $scope.startLocation = {lat:data.coords.latitude, long:data.coords.longitude};
- //   });
-
-
-
-};
-
-
-  
-})
-
-.controller('ChatsCtrl', function ($scope, Chats) {
-	// With the new view caching in Ionic, Controllers are only called
-	// when they are recreated or on app start, instead of every page change.
-	// To listen for when this page is active (for example, to refresh data),
-	// listen for the $ionicView.enter event:
-	//
-	//$scope.$on('$ionicView.enter', function(e) {
-	//});
-
-	$scope.chats = Chats.all();
-	$scope.remove = function (chat) {
-		Chats.remove(chat);
-	}
-})
-
-.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-	$scope.chat = Chats.get($stateParams.chatId);
-})
-
-.directive('imageSearch', function (imageSeachFactory) {
-	return {
-		scope: {
-			keyword: '@keyword'
-		},
-		link: function (scope, element) {
-			var keyword = scope.keyword;
-			imageSeachFactory.getImage(keyword)
-				.success(function (data, status, headers, config) {
-					element.append('<img src="' + data.items[0].link + '" style="width: 100%; height: auto" />');
-				})
-		},
-		template: '<div></div>'
-	}
-})
-
-.controller('RouteCtrl', function($scope, $ionicSlideBoxDelegate, $q, routes, what3words) {
-  routes.getWayPoints('37.7738571,-122.4102823', '37.7891231,-122.4173545').then(function(wayPoints) {
-    var deferreds = [];
-    for (var wayPointIndex = 0; wayPointIndex < wayPoints.length; wayPointIndex++) {
-      var wayPoint = wayPoints[wayPointIndex];
-      deferreds.push(what3words.getWords(WHAT3WORDS_API_KEY, wayPoint.lat, wayPoint.lon));
-    }
-    
-    $q.all(deferreds).then(function(wordLists) {
-      console.log(wayPoints);
-      console.log(wordLists);
-      $scope.data = {};
-      $scope.data.wayPoints = [];
-      for (var wayPointIndex = 0; wayPointIndex < wayPoints.length; wayPointIndex++) {
-        var wayPoint = wayPoints[wayPointIndex];
-        var words = wordLists[wayPointIndex];
-        $scope.data.wayPoints.push({lat: wayPoint.lat,
-                                    lon: wayPoint.lon,
-                                    bearing: wayPoint.bearing,
-                                    words: words});
-      }
-      console.log($scope.data.wayPoints);
-      $ionicSlideBoxDelegate.update();
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
     });
-  });
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
